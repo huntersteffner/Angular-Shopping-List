921449e473efedeebca3fb9ba27adb8c26ef0a46
huntersteffner <huntersteffner@gmail.com>
2023-03-08 14:15:04 -0500

    Click recipe works

--- src/app/recipe-list/recipe-list.component.ts
+++ src/app/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipes/recipe.model';
 
 @Component({
@@ -7,6 +7,7 @@ import { Recipe } from '../recipes/recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>()
   recipes: Recipe[] = [
     new Recipe('A test recipe', 'This is a test', 'https://dynl.mktgcdn.com/p/hxJgH_gPUGuHQPqGidqaJNMl9pbQqLO7esOuNzfyw8o/496x344.png')
   ]
@@ -14,6 +15,10 @@ export class RecipeListComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe)
   }
 
 }
