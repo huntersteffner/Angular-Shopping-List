a44db6ac668904e28cb638d05c675085fcda16d6
Matthew Hunter Steffner <huntersteffner@gmail.com>
2023-04-13 16:02:33 -0400

    Started ingredients

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 import { Recipe } from "./recipe.model";
 
 export class RecipeService {
@@ -6,8 +7,16 @@ export class RecipeService {
 
 
     private recipes: Recipe[] = [
-        new Recipe('A test recipe', 'This is a test', 'https://dynl.mktgcdn.com/p/hxJgH_gPUGuHQPqGidqaJNMl9pbQqLO7esOuNzfyw8o/496x344.png'),
-        new Recipe('Second Test', 'Yes, it\'s a test','https://dynl.mktgcdn.com/p/hxJgH_gPUGuHQPqGidqaJNMl9pbQqLO7esOuNzfyw8o/496x344.png')
+        new Recipe('Pizza', 'Pepperoni', 'https://dynl.mktgcdn.com/p/hxJgH_gPUGuHQPqGidqaJNMl9pbQqLO7esOuNzfyw8o/496x344.png',[
+            new Ingredient('Pepperoni',30),
+            new Ingredient('Dough',2)
+        ]),
+        new Recipe('Taco', 'Orale','https://images-gmi-pmc.edge-generalmills.com/e59f255c-7498-4b84-9c9d-e578bf5d88fc.jpg',[
+            new Ingredient('Tortilla',1),
+            new Ingredient('Carnitas',5),
+            new Ingredient('Crema',3),
+
+        ])
       ]
 
       getRecipes() {
