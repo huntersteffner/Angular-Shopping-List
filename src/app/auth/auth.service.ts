3e1b9018f8be3322ec6c0af19f54da54f65f1cd2
Matthew Hunter Steffner <hunter.steffner@electrolux.com>
2023-05-16 13:18:06 -0400

    Error handling

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { catchError, throwError } from "rxjs";
 import { environment } from "src/environments/environment";
@@ -24,17 +24,7 @@ export class AuthService {
             password: password,
             returnSecureToken: true
         }
-        ).pipe(catchError(errorRes => {
-            let errorMessage = 'An unknown error occurred.'
-            if(!errorRes.error || !errorRes.error.error) {
-                return throwError(errorMessage)
-            }
-            switch (errorRes.error.error.message) {
-                case 'EMAIL_EXISTS': errorMessage = 'This email already exists.'
-            }
-
-            return throwError(errorMessage)
-        }))
+        ).pipe(catchError(this.handleError))
     }
 
     login(email: string, password: string) {
@@ -42,6 +32,24 @@ export class AuthService {
             email: email,
             password: password,
             returnSecureToken: true
-        })
+        }).pipe(catchError(this.handleError))
+    }
+
+    private handleError(errorRes: HttpErrorResponse) {
+        let errorMessage = 'An unknown error occurred.'
+            if(!errorRes.error || !errorRes.error.error) {
+                return throwError(errorMessage)
+            }
+            switch (errorRes.error.error.message) {
+                case 'EMAIL_EXISTS': errorMessage = 'This email already exists.'
+                break
+                case 'EMAIL_NOT_FOUND': errorMessage = 'No existing profiles with this email address.'
+                break
+                case 'INVALID_PASSWORD': errorMessage = 'Incorrect Password'
+                break
+            }
+
+            return throwError(errorMessage)
+
     }
 }
