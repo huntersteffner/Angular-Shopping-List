b90059738cc17072cc88aa8cb64d9e7379f8ecb5
Matthew Hunter Steffner <hunter.steffner@electrolux.com>
2023-05-16 10:55:07 -0400

    Auth login

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,14 +1,16 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { catchError, throwError } from "rxjs";
 import { environment } from "src/environments/environment";
 
-interface AuthResponseData {
+export interface AuthResponseData {
     kind: string
     idToken: string
     email: string
     refreshToken: string
     expiresIn: string
     localId: string
+    registered?: boolean
 }
 
 @Injectable({providedIn: 'root'})
@@ -22,6 +24,24 @@ export class AuthService {
             password: password,
             returnSecureToken: true
         }
-        )
+        ).pipe(catchError(errorRes => {
+            let errorMessage = 'An unknown error occurred.'
+            if(!errorRes.error || !errorRes.error.error) {
+                return throwError(errorMessage)
+            }
+            switch (errorRes.error.error.message) {
+                case 'EMAIL_EXISTS': errorMessage = 'This email already exists.'
+            }
+
+            return throwError(errorMessage)
+        }))
+    }
+
+    login(email: string, password: string) {
+        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + environment.firebaseAPIKey, {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        })
     }
 }
