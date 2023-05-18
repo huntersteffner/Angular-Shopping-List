6d3c26ee217841d8952e0790c57b67f80a7afe98
Matthew Hunter Steffner <hunter.steffner@electrolux.com>
2023-05-18 08:33:38 -0400

    Storage interceptor

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -20,20 +20,19 @@ export class DataStorageService {
     }
 
     fetchRecipes() {
-        return this.authService.user.pipe(take(1), exhaustMap(user => {
-            return this.http.get<Recipe[]>(environment.firebaseAPIUrl + '/recipes.json',
-            {
-                params: new HttpParams().set('auth', user.token)
-            }
-            )
-        }), map(recipes => {
-            return recipes.map(recipe => {
-                return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients: []}
-            })
-        }),
-        tap(recipes => {
-            this.recipeService.setRecipes(recipes)
-        }))
+        
+            return this.http.get<Recipe[]>(environment.firebaseAPIUrl + '/recipes.json').pipe(
+                map(recipes => {
+                    return recipes.map(recipe => {
+                        return {
+                            ...recipe,
+                            ingredients: recipe.ingredients ? recipe.ingredients : []
+                        }
+                    })
+                }),
+                tap(recipes => {
+                    this.recipeService.setRecipes(recipes)
+                }))
         
     }
 }
