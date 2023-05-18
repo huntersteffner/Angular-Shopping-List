ecf5e413800bd62ba5d676fb0d384d49cc80dd87
Matthew Hunter Steffner <hunter.steffner@electrolux.com>
2023-05-18 09:32:48 -0400

    Auto login

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
 import { BehaviorSubject, catchError, Subject, tap, throwError } from "rxjs";
 import { environment } from "src/environments/environment";
 import { User } from "./user.model";
@@ -19,7 +20,7 @@ export class AuthService {
     user = new BehaviorSubject<User>(null)
     
 
-    constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient, private router: Router) {}
 
     signup(email: string, password: string) {
         return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + environment.firebaseAPIKey,
@@ -43,12 +44,38 @@ export class AuthService {
         }))
     }
 
+    logout() {
+        this.user.next(null)
+        this.router.navigate(['/auth'])
+    }
+
     private handleAuthentication(email: string, userId: string, token: string, expiresIn: number) {
         const expirationDate = new Date(new Date().getTime() + expiresIn *1000)
 
-            const user = new User(email, userId, token, expirationDate)
+        const user = new User(email, userId, token, expirationDate)
 
-            this.user.next(user)
+        this.user.next(user)
+
+        localStorage.setItem('userData', JSON.stringify(user))
+    }
+
+    autoLogin() {
+        const userData: {
+            email: string
+            id: string
+            _token: string
+            _tokenExpirationDate: string
+        } = JSON.parse(localStorage.getItem('userData'))
+        if(!userData) {
+            return
+        }
+
+        const loadedUser = new User(userData.email, userData.id, userData._token, new Date(userData._tokenExpirationDate))
+
+        if(loadedUser.token) {
+            this.user.next(loadedUser)
+            
+        }
     }
 
     private handleError(errorRes: HttpErrorResponse) {
